55dcd45625f33dbfb37a2efa05a487ae9631b305
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-27 17:37:27 +0900

    database 생성

--- be01/ex00/models/index.js
+++ be01/ex00/models/index.js
@@ -5,7 +5,9 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")[env];
+const express = require("express");
+const morgan = require("morgan");
 const db = {};
 
 let sequelize;
@@ -42,5 +44,26 @@ Object.keys(db).forEach((modelName) => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+const app = express();
+app.set("port", process.env.PORT || 4242);
+
+sequelize //
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database connected successfully.");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
 
 module.exports = db;
